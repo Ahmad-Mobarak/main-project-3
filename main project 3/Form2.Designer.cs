2e9e9c9c0dab44bb0231eec8dccf7725a39145f1
Ahmad-Mobarak <ahmadmobarak090678@gmail.com>
2023-04-29 20:50:29 +0300

    m

--- main project 3/Form2.Designer.cs
+++ main project 3/Form2.Designer.cs
@@ -85,7 +85,7 @@
             // button4
             // 
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(306, 378);
+            this.button4.Location = new System.Drawing.Point(317, 378);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(217, 40);
             this.button4.TabIndex = 83;
